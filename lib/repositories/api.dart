cf7ded18abaf55db9f5f31d4006749fdd86cccd4
pool <pool@skipapp.com.au>
2021-04-10 17:32:16 +0800

    Add get method for API calls

--- lib/repositories/api.dart
+++ lib/repositories/api.dart
@@ -9,6 +9,12 @@ class ApiClient {
   final String hostname = "http://10.0.2.2:8000";
   final log = Logger("ApiClient");
 
+  dynamic get({@required String path}) async {
+    var response = await http.get("$hostname$path");
+
+    return json.decode(response.body);
+  }
+
   Future<Map<String, Object>> post({
     @required String path,
     Map<String, Object> body,
@@ -19,15 +25,19 @@ class ApiClient {
     log.info("Sending request to $url with content: $serializedContent");
     var response = await http.post(
       url,
-      headers: {
-        'Content-type': 'application/json',
-        'Accept': 'application/json',
-      },
+      headers: _requestHeaders(),
       body: serializedContent,
     );
     log.info(
         "Received request from $url with status ${response.statusCode} and content: ${response.body}");
 
     return json.decode(response.body);
+  }
+
+  Map<String, String> _requestHeaders() {
+    return {
+      'Content-type': 'application/json',
+      'Accept': 'application/json',
+    };
   }
 }
