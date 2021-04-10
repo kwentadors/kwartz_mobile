aa28833eef2160075d352accd16f79d53b616140
pool <pool@skipapp.com.au>
2021-04-10 18:40:12 +0800

    Minor refactoring on repository

--- lib/repositories/api.dart
+++ lib/repositories/api.dart
@@ -6,11 +6,14 @@ import 'package:http/http.dart' as http;
 import 'package:logging/logging.dart';
 
 class ApiClient {
-  final String hostname = "http://10.0.2.2:8000";
-  final log = Logger("ApiClient");
+  final HOSTNAME = "http://10.0.2.2:8000";
+  final LOGGER = Logger("ApiClient");
 
   dynamic get({@required String path}) async {
-    var response = await http.get("$hostname$path");
+    final url = "$HOSTNAME$path";
+    LOGGER.info("Sending request to $url");
+    final response = await http.get(url);
+    _logResponse(response);
 
     return json.decode(response.body);
   }
@@ -20,18 +23,22 @@ class ApiClient {
     Map<String, Object> body,
   }) async {
     String serializedContent = json.encode(body);
-    var url = "$hostname$path";
+    final url = "$HOSTNAME$path";
 
-    log.info("Sending request to $url with content: $serializedContent");
-    var response = await http.post(
+    LOGGER.info("Sending request to $url with content: $serializedContent");
+    final response = await http.post(
       url,
       headers: _requestHeaders(),
       body: serializedContent,
     );
-    log.info(
-        "Received request from $url with status ${response.statusCode} and content: ${response.body}");
+    _logResponse(response);
 
     return json.decode(response.body);
+  }
+
+  void _logResponse(response) {
+    LOGGER.info(
+        "Received request from ${response.request.url} with status ${response.statusCode} and content: ${response.body}");
   }
 
   Map<String, String> _requestHeaders() {
