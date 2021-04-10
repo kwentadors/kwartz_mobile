723b875d71a4dd9d725bd2886c9477af7f16b1b3
pool <pool@skipapp.com.au>
2021-04-10 18:40:38 +0800

    Extract account portion of journal entry section

--- lib/molecules/journal_entry.dart
+++ lib/molecules/journal_entry.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/material.dart';
+import 'journal_entry_account.dart';
 import '../blocs/transaction_bloc.dart';
 import '../models/transaction.dart';
 
@@ -69,58 +70,6 @@ class _AmountInputState extends State<AmountInput> {
             this.widget.entry.copyWith(amount: double.parse(value));
         var journalEntryIndex = (this.widget.key as ValueKey).value;
 
-        if (journalEntry.type == JournalEntryType.DEBIT) {
-          BlocProvider.of<TransactionBloc>(context)
-              .add(UpdateDebitEntry(journalEntryIndex, journalEntry));
-        } else {
-          BlocProvider.of<TransactionBloc>(context)
-              .add(UpdateCreditEntry(journalEntryIndex, journalEntry));
-        }
-      },
-    );
-  }
-}
-
-class AccountNameInput extends StatefulWidget {
-  final JournalEntry entry;
-
-  const AccountNameInput({Key key, @required this.entry}) : super(key: key);
-
-  @override
-  _AccountNameInputState createState() => _AccountNameInputState();
-}
-
-class _AccountNameInputState extends State<AccountNameInput> {
-  FinancialAccount value;
-
-  @override
-  Widget build(BuildContext context) {
-    List<FinancialAccount> accounts = [
-      FinancialAccount('Cash on Hand'),
-      FinancialAccount('Income'),
-      FinancialAccount('Expense - Personal')
-    ];
-
-    return DropdownButtonFormField<FinancialAccount>(
-      value: this.widget.entry?.account,
-      validator: (value) {
-        if (value == null) {
-          return 'Select an account';
-        }
-        return null;
-      },
-      isDense: true,
-      isExpanded: true,
-      hint: Text('Account'),
-      items: accounts
-          .map((account) => DropdownMenuItem(
-                child: Text(account?.name ?? "Empty"),
-                value: account,
-              ))
-          .toList(),
-      onChanged: (value) {
-        var journalEntry = this.widget.entry.copyWith(account: value);
-        var journalEntryIndex = (this.widget.key as ValueKey).value;
         if (journalEntry.type == JournalEntryType.DEBIT) {
           BlocProvider.of<TransactionBloc>(context)
               .add(UpdateDebitEntry(journalEntryIndex, journalEntry));
