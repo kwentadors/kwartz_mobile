57607bac74556e60cba87b1edebace14bb93a30f
pool <pool@skipapp.com.au>
2021-05-26 20:21:40 +0800

    Implement aggregate balance and change percent on asset reports

--- lib/modules/asset_ledger/models/asset_report.dart
+++ lib/modules/asset_ledger/models/asset_report.dart
@@ -7,9 +7,15 @@ class AssetReport {
     @required this.groups,
   });
 
-  double get balance => 523720.72;
+  double get balance =>
+      this.groups.fold(0, (sum, entry) => sum + (entry.balance ?? 0));
 
-  double get changePercent => 2.57;
+  double get changePercent =>
+      this.groups.fold(
+          0,
+          (result, group) =>
+              result + ((group.changePercent ?? 0) * group.balance)) /
+      this.balance;
 }
 
 class AssetReportGroup {
@@ -21,6 +27,17 @@ class AssetReportGroup {
     @required this.id,
     @required this.name,
   });
+
+  double get balance =>
+      this.entries.fold(0, (sum, entry) => sum + entry.amount);
+
+  double get changePercent => this.balance == 0
+      ? 0.0
+      : this.entries.fold(
+              0,
+              (result, entry) =>
+                  result + ((entry.changePercent ?? 0) * entry.amount)) /
+          this.balance;
 }
 
 class AssetReportGroupEntry {
