884d6a33472378c1602cb9e886da911b85b0ea6e
pool <pool@skipapp.com.au>
2021-07-02 20:13:17 +0800

    Remove stale data

--- lib/modules/transaction/screens/asset_ledger_page.dart
+++ lib/modules/transaction/screens/asset_ledger_page.dart
@@ -1,57 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:kwartz_mobile/modules/asset_ledger/widgets/income_expense_summary_card.dart';
 import 'common/kwartz_bottom_nav_bar.dart';
 import '../../asset_ledger/widgets/asset_list.dart';
 import '../../asset_ledger/widgets/asset_card.dart';
 
 class AssetLedgerPage extends StatelessWidget {
-  final _data = [
-    {
-      'name': 'Cash and equivalents',
-      'entries': [
-        {
-          "name": "Savings-BDO",
-          "description": "Account Number: XXXXXXX8025",
-          "amount": 27899.85
-        },
-        {
-          "name": "Savings-Unionbank",
-          "description": "Account Number: XXXXXXX9970",
-          "amount": 3691.12
-        },
-      ]
-    },
-    {
-      'name': 'Paper Investments',
-      'entries': [
-        {
-          "name": "Stock Market - COL",
-          "description": null,
-          "amount": 5000.00,
-        },
-        {
-          "name": "Stock Market - First Metro Sec",
-          "description": null,
-          "amount": 15000.00
-        },
-      ],
-    },
-    {
-      'name': 'Property',
-      'entries': [
-        {
-          "name": "Tayud House & Lot",
-          "description": null,
-          "amount": 285000.00,
-        },
-        {
-          "name": "Bloq Residences - Unit 405",
-          "description": null,
-          "amount": 15000.00
-        },
-      ],
-    }
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -59,15 +12,14 @@ class AssetLedgerPage extends StatelessWidget {
         title: Text("Ledger"),
         backgroundColor: Theme.of(context).colorScheme.primary,
       ),
-      body: ListLedgerBody(data: _data),
+      body: ListLedgerBody(),
       bottomNavigationBar: KwartzBottomNavigationBar(KwartzNavigation.Reports),
     );
   }
 }
 
 class ListLedgerBody extends StatelessWidget {
-  final List<Map<String, dynamic>> data;
-  const ListLedgerBody({Key key, this.data}) : super(key: key);
+  const ListLedgerBody({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -81,7 +33,7 @@ class ListLedgerBody extends StatelessWidget {
             child: AssetsCard(),
           ),
           SizedBox(height: 20),
-          Expanded(child: AssetList(data: data))
+          Expanded(child: AssetList())
         ],
       ),
     );
