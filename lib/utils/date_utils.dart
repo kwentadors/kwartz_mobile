5016b95c7efc495f86a58be201219985fd1e2cab
pool <pool@skipapp.com.au>
2021-08-13 20:05:57 +0800

    Add method to resolve month index

--- lib/utils/date_utils.dart
+++ lib/utils/date_utils.dart
@@ -17,4 +17,33 @@ class DateUtils {
       'Dec,'
     ][index];
   }
+
+  static int monthIndex(String monthName) {
+    switch (monthName.toLowerCase()) {
+      case "january":
+        return 1;
+      case "february":
+        return 2;
+      case "march":
+        return 3;
+      case "april":
+        return 4;
+      case "may":
+        return 5;
+      case "june":
+        return 6;
+      case "july":
+        return 7;
+      case "august":
+        return 8;
+      case "september":
+        return 9;
+      case "october":
+        return 10;
+      case "november":
+        return 11;
+      case "december":
+        return 12;
+    }
+  }
 }
